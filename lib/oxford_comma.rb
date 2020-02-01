61c372b76431d951afe4f6268a7346ed9eaef4a3
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-01 21:02:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,3 @@
-require 'pry'
-
-
-def oxford_comma(array)
-  if array.size == 1
-    array.join
-
-  elsif array.size == 2
-    array.join(" and ")
-
-
-  elsif array.size == 3
-    array.map do |index| S
-
-
-  end
-
+def oxford_comma
 
 end
