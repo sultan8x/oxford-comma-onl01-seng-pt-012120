341662de2706f79c492c52404c5449bb4377622b
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-01 21:35:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,10 @@
 
 def oxford_comma(array)
     if array.size < 2
-    #   array.join
-    #
-    # elsif array.size == 2
-      array.join(" and ")
+      array.join
+
+    elsif array.size == 2
+      array.join (" and ")
 
     else array.size == 3
       last_word = array.pop
