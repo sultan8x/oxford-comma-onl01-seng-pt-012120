4b76cfe6a62b2cc1513d95ad2c6e06ea7f6e2644
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-01 21:23:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,19 +2,6 @@
 
 
 def oxford_comma(array)
-  array.size == 1
-  return array
+  array.size
 
-  array.size = 2
-  array.join (" and ")
-
-
-elsif array.length > 2
-      last_word = array.pop
-    new_array = array.join(", ")
-      new_array << ", and #{last_word}"
-  return new_list
-
-  end
-
- end
+end
