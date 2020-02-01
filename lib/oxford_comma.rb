864c2ad5da0cb2796166aa19b1e39cc35cbfda8b
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-01 21:41:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+# ["kiwi", "durian", "starfruit"]
+
+
 def oxford_comma(array)
+    if array.size == 1
+      array.join
+
+    elsif array.size ==  2
+      array.join(" and ")
+
+    else array.size <= 3
+      last_word = array.pop
+      updated_array = array.join(", ")
+      updated_array << ", and #{last_word}"
+      return updated_array
+    end
 
 end
