6d05f6cd73d5168c8ae6612c7b02613dc1f6ef17
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-01 21:38:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,17 @@
-require 'pry'
+# ["kiwi", "durian", "starfruit"]
+
 
 def oxford_comma(array)
-    if array.size == 1
+    if array.size < 2
       array.join
 
     elsif array.size == 2
-      array.join(" and ")
+      array.join (" and ")
 
     else array.size == 3
       last_word = array.pop
       new_array = array.join(", ")
-      new_array << ", and #{last_word}
+      new << ", and #{last_word}
       return new_array
     end
 
