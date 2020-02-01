cd1400bb1510689be8cc0a3ed6eb1630190da3b0
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-01 20:29:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
+
 def oxford_comma(array)
+  if array.size == 1
+    array.join
+
+  elsif array.size == 2
+    array.join(" and ")
+
+
+  elsif array.size == 3
+      array.join(", ")
+      array.split
+      array.delete_at(2)
+      array.join << " and starfruit"
+
+  end
+
 
 end
